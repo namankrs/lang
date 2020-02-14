95570dc20eda3550f363505ea0714dd1b8b6cf33
kanishka.k.aux <kanishka.k.aux@go-jek.com>
2020-02-14 23:35:31 +0530

    Add rule for whitespace characters.

--- src/my_lang/core.clj
+++ src/my_lang/core.clj
@@ -3,7 +3,7 @@
 
 (defparser calculator-parser (clojure.java.io/resource "myparser.bnf"))
 
-(->> (calculator-parser "1+2+3")
+(->> (calculator-parser "1 + 2 + 3 ")
      (insta/transform
        {:sum    +, :sub -, :mul *, :div /, :exp identity,
         :number clojure.edn/read-string}))
