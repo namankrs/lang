74e12bfa422da8a5d107b0ff371b6765f68a0d0d
Naman <namansha@Namans-MacBook-Pro.local>
2020-02-20 14:27:52 +0530

    Add assignment

--- src/my_lang/core.clj
+++ src/my_lang/core.clj
@@ -1,17 +1,26 @@
 (ns my-lang.core
   (:require [instaparse.core :as insta :refer :all]))
 
-(defparser calculator-parser (clojure.java.io/resource "myparser.bnf"))
+(defparser calculator-parser (clojure.java.io/resource "calculatorParser.bnf"))
+(defparser language-parser (clojure.java.io/resource "language-parser.bnf"))
 
-(defn transformer [exp]
+
+(defn calculator [exp]
   (->> (calculator-parser exp)
        (insta/transform
-         {:add +, :sub -, :mul *, :div /, :exp identity,
-          :number clojure.edn/read-string})))
+        {:add +, :sub -, :mul *, :div /, :exp identity
+         :number clojure.edn/read-string})))
 
-(calculator-parser "")
-(transformer "2*2")
-;(defn -main []
-;  (println (transformer)))
+(defn language [exp]
+  (->> (language-parser exp)
+       (insta/transform
+        {:assignment "def", :value clojure.edn/read-string})))
+
+(calculator "2*2")
+(language "x=4\ny=6")
+
+(defn -main []
+  (do []
+      (println (language-parser "x=4"))))
 
 
