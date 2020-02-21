3c5c2ea4c4a141c3e0c9308fe8d4d00e282112b2
Naman <snaman80@gmail.com>
2020-02-21 12:52:42 +0530

    Integrate repl.

--- src/my_lang/core.clj
+++ src/my_lang/core.clj
@@ -24,11 +24,23 @@
          :defined-var (comp eval clojure.edn/read-string )})))
 
 (calculator "2*2")
-(language-parser "x=4\ny=3\nx+y")
-(language "x=4\ny=3\nx+y")
+(language-parser "x=4\ny=3\nx+y\ny+2")
+(language "x=4\ny=3\nx+y\ny+2")
+
+(defn repl []
+  (ns my-lang.core)
+  (do
+    (print "my-lang> ")
+    (flush))
+  (let [input (read-line)]
+    (println (first (language input)))
+    (recur)))
 
 (defn -main []
   (do []
-      (println (language-parser "x=4"))))
+  (println "Namaskara")
+  (println "===============")
+  (flush)
+  (repl)))
 
 
